028b61ab2aa9aabbf28c9c1314d981d458379676
keerthi <keerthi@htic.iitm.ac.in>
2020-09-26 15:03:42 +0530

    changes for vector layer and basic polygon drawing

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import Map from 'ol/Map';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
 import Zoomify from 'ol/source/Zoomify';
+import Draw from 'ol/interaction/Draw';
+import  VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
 
 var imgWidth = 4000;
 var imgHeight = 3000;
@@ -17,22 +20,26 @@ var source = new Zoomify({
 });
 var extent = source.getTileGrid().getExtent();
 
-var retinaPixelRatio = 2;
-var retinaSource = new Zoomify({
-  url: zoomifyUrl,
-  size: [imgWidth, imgHeight],
-  crossOrigin: 'anonymous',
-  zDirection: -1, // Ensure we get a tile with the screen resolution or higher
-  tilePixelRatio: retinaPixelRatio, // Display retina tiles
-  tileSize: 256 / retinaPixelRatio, // from a higher zoom level
-});
+// var retinaPixelRatio = 2;
+// var retinaSource = new Zoomify({
+//   url: zoomifyUrl,
+//   size: [imgWidth, imgHeight],
+//   crossOrigin: 'anonymous',
+//   zDirection: -1, // Ensure we get a tile with the screen resolution or higher
+//   tilePixelRatio: retinaPixelRatio, // Display retina tiles
+//   tileSize: 256 / retinaPixelRatio, // from a higher zoom level
+// });
 
 var layer = new TileLayer({
   source: source,
 });
 
+var vector = new VectorLayer({
+  source: new VectorSource({wrapX: false}),
+});
+
 var map = new Map({
-  layers: [layer],
+  layers: [layer,vector],
   target: 'map',
   view: new View({
     // adjust zoom levels to those provided by the source
@@ -44,12 +51,24 @@ var map = new Map({
 });
 map.getView().fit(extent);
 
-var control = document.getElementById('zoomifyProtocol');
+var control = document.getElementById('dd_interaction');
+
+var draw;
 control.addEventListener('change', function (event) {
   var value = event.currentTarget.value;
-  if (value === 'zoomify') {
-    layer.setSource(source);
-  } else if (value === 'zoomifyretina') {
-    layer.setSource(retinaSource);
+  if (value == "polygon") {
+    draw = new Draw({
+      source: vector.getSource(),
+      type: "Polygon",
+    });
+    map.addInteraction(draw);
   }
+  else {
+    map.removeInteraction(draw);
+  }
+  // if (value === 'zoomify') {
+  //   layer.setSource(source);
+  // } else if (value === 'zoomifyretina') {
+  //   layer.setSource(retinaSource);
+  // }
 });
